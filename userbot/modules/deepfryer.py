d6cd0972f8cf4fab4bb5f8957188df3b841b34a6
Angga <angga@linuxmail.org>
2020-08-15 04:13:18 +0700

    deepfryer: don't crash if bot dead
    Change-Id: Ia92150922e70cdf7bae3ad2eef13243dae3c53fb
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/deepfryer.py
+++ userbot/modules/deepfryer.py
@@ -1,4 +1,5 @@
 import os
+from asyncio.exceptions import TimeoutError
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
@@ -23,43 +24,48 @@ async def _(fry):
         await fry.edit("`Reply to actual user...`")
         return
     chat = "@image_deepfrybot"
-    message_id_to_reply = fry.message.reply_to_msg_id
-    async with fry.client.conversation(chat) as conv:
-        try:
-            msg = await conv.send_message(reply_message)
-            if level:
-                m = f"/deepfry {level}"
-                msg_level = await conv.send_message(m, reply_to=msg.id)
-                r = await conv.get_response()
-            response = await conv.get_response()
-            """ - don't spam notif - """
-            await bot.send_read_acknowledge(conv.chat_id)
-        except YouBlockedUserError:
-            await fry.reply("`Please unblock` @image_deepfrybot`...`")
-            return
-        if response.text.startswith("Forward"):
-            await fry.edit("`Please disable your forward privacy setting...`")
-        else:
-            downloaded_file_name = await fry.client.download_media(
-                response.media, TEMP_DOWNLOAD_DIRECTORY
-            )
-            await fry.client.send_file(
-                fry.chat_id,
-                downloaded_file_name,
-                force_document=False,
-                reply_to=message_id_to_reply,
-            )
-            """ - cleanup chat after completed - """
+    try:
+        message_id_to_reply = fry.message.reply_to_msg_id
+        async with fry.client.conversation(chat) as conv:
             try:
-                msg_level
-            except NameError:
-                await fry.client.delete_messages(conv.chat_id, [msg.id, response.id])
+                msg = await conv.send_message(reply_message)
+                if level:
+                    m = f"/deepfry {level}"
+                    msg_level = await conv.send_message(m, reply_to=msg.id)
+                    r = await conv.get_response()
+                response = await conv.get_response()
+                """ - don't spam notif - """
+                await bot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                await fry.reply("`Please unblock `@image_deepfrybot`...`")
+                return
+            if response.text.startswith("Forward"):
+                await fry.edit("`Please disable your forward privacy setting...`")
             else:
-                await fry.client.delete_messages(
-                    conv.chat_id, [msg.id, response.id, r.id, msg_level.id]
+                downloaded_file_name = await fry.client.download_media(
+                    response.media, TEMP_DOWNLOAD_DIRECTORY
                 )
-    await fry.delete()
-    return os.remove(downloaded_file_name)
+                await fry.client.send_file(
+                    fry.chat_id,
+                    downloaded_file_name,
+                    force_document=False,
+                    reply_to=message_id_to_reply,
+                )
+                """ - cleanup chat after completed - """
+                try:
+                    msg_level
+                except NameError:
+                    await fry.client.delete_messages(
+                        conv.chat_id, [msg.id, response.id]
+                    )
+                else:
+                    await fry.client.delete_messages(
+                        conv.chat_id, [msg.id, response.id, r.id, msg_level.id]
+                    )
+        await fry.delete()
+        return os.remove(downloaded_file_name)
+    except TimeoutError:
+        return await event.edit("`Error: `@image_deepfrybot` is not responding!`")
 
 
 CMD_HELP.update(
