a0362b72431058b14e137cec986bf713b25d3893
Angga <angga@linuxmail.org>
2020-09-03 23:01:10 +0700

    nsfw: add some nsfw stuff
    * .pgif
      Usage: Get pussy gif.
    * .pjpg
      Usage: Get pussy image.
    * .pat
      Usage: Get random pat gif.
    * .cum
      Usage: Get random cum gif.
    
    NB : Need .update deploy
    
    Change-Id: I7486f499f3198860b80d60fade4778302894d823
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/nsfw.py
+++ userbot/modules/nsfw.py
@@ -1,13 +1,23 @@
 # Credits to https://t.me/TheHardGamer
 # Edited by @AnggaR96s
+import asyncio
+import json
 import os
+import random
 import urllib
 from asyncio import sleep
+from urllib.parse import quote as urlencode
 
+import aiohttp
+import nekos
 import requests
+from jikanpy import Jikan
 
 from userbot import CMD_HELP, bot
 from userbot.events import register
+
+_pats = []
+jikan = Jikan()
 
 
 @register(outgoing=True, pattern=r"^\.boobs(?: |$)(.*)")
@@ -38,11 +48,66 @@ async def butts(e):
     await e.delete()
 
 
+@register(outgoing=True, pattern=r"^.pat(?: |$)")
+async def pat(e):
+    global _pats
+
+    url = "https://headp.at/js/pats.json"
+    if not _pats:
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url) as raw_resp:
+                resp = await raw_resp.text()
+        _pats = json.loads(resp)
+    pats = _pats
+
+    pats = [i for i in pats if os.path.splitext(i)[1] == ".gif"]
+
+    pat = random.choice(pats)
+    link = f"https://headp.at/pats/{urlencode(pat)}"
+
+    await asyncio.wait([e.respond(file=link, reply_to=e.reply_to_msg_id), e.delete()])
+
+
+@register(outgoing=True, pattern=r"^\.pgif(?: |$)(.*)")
+async def pussyg(e):
+    await e.edit("`Finding some pussy gifs...`")
+    await sleep(2)
+    target = "pussy"
+    await bot.send_file(e.chat_id, nekos.img(target), reply_to=e.reply_to_msg_id)
+    await e.delete()
+
+
+@register(outgoing=True, pattern=r"^\.pjpg(?: |$)(.*)")
+async def pussyp(e):
+    await e.edit("`Finding some pussy pics...`")
+    await sleep(2)
+    target = "pussy_jpg"
+    await bot.send_file(e.chat_id, nekos.img(target), reply_to=e.reply_to_msg_id)
+    await e.delete()
+
+
+@register(outgoing=True, pattern=r"^\.cum(?: |$)(.*)")
+async def cum(e):
+    await e.edit("`Finding some cum gifs...`")
+    await sleep(2)
+    target = "cum"
+    await bot.send_file(e.chat_id, nekos.img(target), reply_to=e.reply_to_msg_id)
+    await e.delete()
+
+
 CMD_HELP.update(
     {
         "nsfw": ">`.boobs`"
         "\nUsage: Get boobs image.\n"
         ">`.butts`"
-        "\nUsage: Get butts image."
+        "\nUsage: Get butts image.\n"
+        ">`.pgif`"
+        "\nUsage: Get pussy gif.\n"
+        ">`.pjpg`"
+        "\nUsage: Get pussy image.\n"
+        ">`.pat`"
+        "\nUsage: Get random pat gif.\n"
+        ">`.cum`"
+        "\nUsage: Get random cum gif."
     }
 )
