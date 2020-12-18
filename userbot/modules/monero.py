cfa17c33fbe2b965ab1b8bbbd5da0b2d289c04c1
Angga <angga@linuxmail.org>
2020-12-19 01:18:47 +0700

    xmr: show coin info
    Change-Id: I4f4c5b6ad9bfd89edfe8cc7ecd1e81e42e7d7628
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/monero.py
+++ userbot/modules/monero.py
@@ -16,13 +16,18 @@ async def xmr(nanopool):
         await nanopool.edit("`Processing..`")
 
     url = f"https://api.nanopool.org/v1/xmr/user/{WALLET}"
+    durl = "https://localmonero.co/blocks/api/get_stats"
     request = requests.get(url)
     parsed = json.loads(request.text)
+    drequest = requests.get(durl)
+    dparsed = json.loads(drequest.text)
 
     try:
         unc = parsed["data"]["unconfirmed_balance"]
         bal = parsed["data"]["balance"]
         hs = parsed["data"]["hashrate"]
+        diff = dparsed["difficulty"]
+        he = dparsed["height"]
     except KeyError:
         return await nanopool.edit("**Wallet not found or API error!!**")
 
@@ -31,7 +36,10 @@ async def xmr(nanopool):
         f"**Details :** [Nanopool](https://xmr.nanopool.org/account/{WALLET})\n"
         f"**Unconfirmed :** `{unc} XMR`\n"
         f"**Balance :** `{bal} XMR`\n"
-        f"**Hashrate :** `{hs} H/s`")
+        f"**Hashrate :** `{hs} H/s`\n\n"
+        "**Coin Status :**\n"
+        f"**Difficulty :** `{diff}\n`"
+        f"**Height :** `{he}`")
 
     await nanopool.edit(result)
 
