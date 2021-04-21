8412f10ccb5a2e9d9603935aeb4682e86b0d7ac9
Angga <angga@linuxmail.org>
2021-04-21 18:07:43 +0700

    memify: helsinki-af
    Change-Id: I3137a2ce7e9d0f76ea71ba1de5cd28b55027b82d
    Signed-off-by: Angga <angga@linuxmail.org>

--- userbot/modules/memify.py
+++ userbot/modules/memify.py
@@ -27,14 +27,14 @@ async def cat_meme(topString, bottomString, filename, endname):
     imageSize = img.size
     # find biggest font size that works
     fontSize = int(imageSize[1] / 9)
-    font = ImageFont.truetype("userbot/utils/MutantAcademyStyle.ttf", fontSize)
+    font = ImageFont.truetype("userbot/utils/helsinki.ttf", fontSize)
     topTextSize = font.getsize(topString)
     bottomTextSize = font.getsize(bottomString)
     while topTextSize[0] > imageSize[0] - \
             20 or bottomTextSize[0] > imageSize[0] - 20:
         fontSize = fontSize - 1
         font = ImageFont.truetype(
-            "userbot/utils/MutantAcademyStyle.ttf", fontSize)
+            "userbot/utils/helsinki.ttf", fontSize)
         topTextSize = font.getsize(topString)
         bottomTextSize = font.getsize(bottomString)
 
@@ -86,7 +86,7 @@ async def cat_meeme(upper_text, lower_text, picture_name, endname):
                 main_image.width))).upper()
     lower_margin = MARGINS[lower_text.count("\n")]
     text_draw = Drawing()
-    text_draw.font = join(getcwd(), "userbot/utils/MutantAcademyStyle.ttf")
+    text_draw.font = join(getcwd(), "userbot/utils/helsinki.ttf")
     text_draw.font_size = 100
     text_draw.text_alignment = "center"
     text_draw.stroke_color = Color("black")
